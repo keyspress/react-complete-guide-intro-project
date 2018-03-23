3e26d916e23ef2edd54b8a90abeea8c05fa26b4c
Kyle Desktop <kylespresser@gmail.com>
2018-03-23 11:30:03 -0400

    trying module css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -75,18 +75,18 @@ class App extends Component {
 
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>It's working</p>
+          <p className={assignedClasses.join(' ')}>It's working</p>
           <button
             style={style}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
